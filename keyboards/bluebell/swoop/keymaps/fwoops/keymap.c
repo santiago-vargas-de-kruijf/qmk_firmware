1f64e78b23c3e13f7913d589525ba097aa7c9846
Santiago Vargas de Kruijf <santiago.vdk@gmail.com>
2025-04-12 11:10:16 +0200

    feat: add combos

--- keyboards/bluebell/swoop/keymaps/fwoops/keymap.c
+++ keyboards/bluebell/swoop/keymaps/fwoops/keymap.c
@@ -33,12 +33,12 @@
 #ifdef OLED_ENABLE
 #    include "demon.c"
 #endif
+
 void oled_render_logo(void) {
     static const char PROGMEM crkbd_logo[] = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
     oled_write_P(crkbd_logo, false);
 }
 
-// -- Probably some other stuff and then --
 
 #ifdef OLED_ENABLE
 bool oled_task_user(void) {
@@ -67,7 +67,27 @@ enum layers {
     _SYMBOL,
     _NAV,
     _FUNC
-}
+};
+
+enum combos {
+    WE_UP,
+    SD_DOWN,
+    DF_BACKSPACE,
+    MV_CAPSLOCK
+};
+
+const uint16_t PROGMEM up[] = {KC_W, KC_E, COMBO_END};
+const uint16_t PROGMEM down[] = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM backspace[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM capslock[] = {LSFT_T(KC_V), RSFT_T(KC_M), COMBO_END};
+
+
+combo_t key_combos[] = {
+   [WE_UP] = COMBO(up, KC_UP),
+   [SD_DOWN] = COMBO(down, KC_DOWN),
+   [DF_BACKSPACE] = COMBO(backspace, KC_BSPC),
+   [MV_CAPSLOCK] = COMBO(capslock, KC_CAPS_LOCK)
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
--- keyboards/bluebell/swoop/keymaps/fwoops/rules.mk
+++ keyboards/bluebell/swoop/keymaps/fwoops/rules.mk
@@ -1,3 +1,4 @@
+COMBO_ENABLE = yes
 WPM_ENABLE = yes  # Enable word per minute counter
 LTO_ENABLE = yes  # Makes hex file smaller
 OLED_ENABLE = yes # Enable OLEDs
